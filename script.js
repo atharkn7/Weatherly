ca0b7b25a034fd43f2654c8451a7eafb787523fb
atharkn7 <athar.kn7@gmail.com>
2025-11-05 01:34:33 +0530

    API info added in JS File

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Using DEMO API Key [Free tier]
+https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=89ea568429f921975234377ad5c8fd63&units=metric
+*/
+
+/* For countries data
+https://restcountries.com/
+*/
+
+/* For cities using country codes from above API
+https://rapidapi.com/wirefreethought/api/geodb-cities
+*/
